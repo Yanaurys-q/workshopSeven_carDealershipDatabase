93cec7bb3f5fdaabf4f86af55b8f84758c49ec55
Yanaurys-q <yanaurysq@gmail.com>
2025-06-08 16:38:50 -0400

    Split combined queries.sql into six individual test script

--- dealerships_by_vehicle.sql
+++ dealerships_by_vehicle.sql
@@ -6,4 +6,4 @@ JOIN inventory i ON d.dealership_id = i.dealership_id
 JOIN vehicles v ON i.VIN = v.VIN
 WHERE v.make = 'Ford'
   AND v.model = 'Mustang'
-  AND v.color = 'Red';
+  AND v.color = 'White';
